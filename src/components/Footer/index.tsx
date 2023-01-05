7b9df61b10d294448cb0e69713bf0f9f1bd85e3b
eduschwert <106620111+eduschwert@users.noreply.github.com>
2023-01-05 12:59:36 -0300

    fix: semicolon prettier

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,10 +1,10 @@
-import React from "react"
-import { StyledFooter } from "./style"
+import React from "react";
+import { StyledFooter } from "./style";
 
 export const Footer = () => {
   return (
     <StyledFooter>
       <p>Close Worker -Todos os direitos reservados - 2023</p>
     </StyledFooter>
-  )
-}
+  );
+};

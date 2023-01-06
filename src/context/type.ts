ff703ea1b7620e3461686e28a003a581cc69e2ea
Antonio <netoifpe@gmail.com>
2023-01-06 15:24:02 -0300

    feat: create the useState userProfile and loadingButton

--- src/context/type.ts
+++ src/context/type.ts
@@ -1,7 +1,19 @@
 import { ReactNode } from "react";
+import { boolean } from "yup";
 
-export interface iUserContext {}
+export interface iUserContext {
+  loadingButton: boolean;
+  userProfile: iUserProfile;
+}
 
 export interface iPropsUserProvider {
   children: ReactNode;
+}
+
+export interface iUserProfile {
+  email: string;
+  name: string;
+  contact: string;
+  avatar: string;
+  id: number;
 }

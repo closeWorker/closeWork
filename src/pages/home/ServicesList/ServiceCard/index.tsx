3c91209574c4b04ba1474a7e4e42a08f8e79b1cd
eduschwert <106620111+eduschwert@users.noreply.github.com>
2023-01-10 09:04:19 -0300

    star rating

--- src/pages/home/ServicesList/ServiceCard/index.tsx
+++ src/pages/home/ServicesList/ServiceCard/index.tsx
@@ -22,7 +22,7 @@ export const ServiceCard = ({ service }: iServiceCardProps) => {
         ratingList.reduce(
           (currentSum, currentRate) => currentSum + currentRate.service_rating,
           0
-        ) / ratingList.length).toFixed(2)
+        ) / ratingList.length)
     : (averageRate = "Sem avaliações no momento");
 
   const whatsNumber = (phoneNumber: string) =>
@@ -36,6 +36,43 @@ export const ServiceCard = ({ service }: iServiceCardProps) => {
           srcLink={service.service_provider_avatar}
           type="modal"
         />
+      </div>
+      <div>
+        <Title type="Headline" colorTitle="blue-1">
+          Nome do prestador
+        </Title>
+        <Title type="Body-600" colorTitle="blue-1">
+          {service.service_provider}
+        </Title>
+      </div>
+      <div>
+        <Title type="Headline" colorTitle="blue-1">
+          Tipo de serviço
+        </Title>
+        <Title type="Body-600" colorTitle="blue-1">
+          {service.kind_of_service}
+        </Title>
+      </div>
+      <div>
+        <Title type="Headline" colorTitle="blue-1">
+          Contato
+        </Title>
+        <Title type="Body-600" colorTitle="blue-1">
+          {service.phone_number}
+        </Title>
+        <a
+          href={`https://api.whatsapp.com/send?phone=${whatsNumber(
+            service.phone_number
+          )}`}
+          target="_blank"
+        >
+          <img src={whatsappIcon} alt="Ícone do whatsappp" />
+        </a>
+      </div>
+      <div>
+        <Title type="Headline" colorTitle="blue-1">
+          Avaliações:
+        </Title>
         {typeof averageRate === "string" ? (
           <Title type="Headline" colorTitle="blue-1">
             {averageRate}
@@ -78,42 +115,10 @@ export const ServiceCard = ({ service }: iServiceCardProps) => {
               />
             )}
             <Title type="Headline" colorTitle="blue-1">
-              ({averageRate.toString().replace(".", ",")})
+              ({averageRate.toFixed(2).replace(".", ",")})
             </Title>
           </StyledStars>
         )}
-      </div>
-      <div>
-        <Title type="Headline" colorTitle="blue-1">
-          Nome do prestador
-        </Title>
-        <Title type="Body-600" colorTitle="blue-1">
-          {service.service_provider}
-        </Title>
-      </div>
-      <div>
-        <Title type="Headline" colorTitle="blue-1">
-          Tipo de serviço
-        </Title>
-        <Title type="Body-600" colorTitle="blue-1">
-          {service.kind_of_service}
-        </Title>
-      </div>
-      <div>
-        <Title type="Headline" colorTitle="blue-1">
-          Contato
-        </Title>
-        <Title type="Body-600" colorTitle="blue-1">
-          {service.phone_number}
-        </Title>
-        <a
-          href={`https://api.whatsapp.com/send?phone=${whatsNumber(
-            service.phone_number
-          )}`}
-          target="_blank"
-        >
-          <img src={whatsappIcon} alt="Ícone do whatsappp" />
-        </a>
       </div>
       <LinkNavigation
         linkTo={`/more_infos/${service.id}`}
